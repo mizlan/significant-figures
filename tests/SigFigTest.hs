9c2ff2efdcb842c48cd77fa3a30f797c4105bf8c
Michael Lan <michaellan202@gmail.com>
2022-08-11 09:06:38 -0700

    more tests

--- tests/SigFigTest.hs
+++ tests/SigFigTest.hs
@@ -34,10 +34,10 @@ singleTermParenTests = testGroup "single terms with parens"
       maybeParse "(2)" @?= Just (mkSFTerm 1 2 0)
   , testCase "parse negative integer w/ double parens" $
       maybeParse "((-3))" @?= Just (mkSFTerm 1 (-3) 0)
-  , testCase "parse sci-not integer" $
-      maybeParse "(-5e7)" @?= Just (mkSFTerm 1 (-5) (-7))
-  , testCase "parse sci-not float w/ double parens" $
-      maybeParse "((5.24e-2))" @?= Just (mkSFTerm 3 524 4)
+  , testCase "parse sci-not integer w/ spaces" $
+      maybeParse "(-5e7   )" @?= Just (mkSFTerm 1 (-5) (-7))
+  , testCase "parse sci-not float w/ double parens and spaces" $
+      maybeParse "(  (5.24e-2) )" @?= Just (mkSFTerm 3 524 4)
   ]
 
 singleOpTests :: TestTree
@@ -48,6 +48,18 @@ singleOpTests = testGroup "single operations"
       maybeParse "3.2 - 4.3" @?= Just (mkSFTerm 2 (-11) 1)
   , testCase "parse multiplication" $
       maybeParse "3 * 2" @?= Just (mkSFTerm 1 6 0)
+  , testCase "parse division with parens" $
+      maybeParse "(4 / 2) / 2" @?= Just (mkSFTerm 1 1 0)
+  ]
+
+orderOfOperations :: TestTree
+orderOfOperations = testGroup "order of operations"
+  [ testCase "addition after multiplication" $
+      maybeParse "1 + 3 * 2" @?= Just (mkSFTerm 1 7 0)
+  , testCase "multiplication before subtraction" $
+      maybeParse "2.1 * 2.0 - 0.3" @?= Just (mkSFTerm 2 (39) 1)
+  , testCase "exp > mul > add" $
+      maybeParse "2.1 + 2.0 * antilog(3.3)" @?= Just (mkSFTerm 1 6 0)
   , testCase "parse division with parens" $
       maybeParse "(4 / 2) / 2" @?= Just (mkSFTerm 1 1 0)
   ]
