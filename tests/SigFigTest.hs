d9b7aaedbc69673e02cf091add2a21e4fdf49e2b
Michael Lan <michaellan202@gmail.com>
2022-08-10 21:21:47 -0700

    MORE TESTS

--- tests/SigFigTest.hs
+++ tests/SigFigTest.hs
@@ -11,10 +11,13 @@ mkSFTerm :: Integer -> Integer -> Integer -> SFTerm
 mkSFTerm sf v s = SFTerm sf (BigDecimal v s)
 
 main :: IO ()
-main = defaultMain unitTests
+main = defaultMain tests
 
-unitTests :: TestTree
-unitTests = testGroup "unit tests"
+tests :: TestTree
+tests = testGroup "basic tests" [singleTermTests, singleTermParenTests, singleOpTests]
+
+singleTermTests :: TestTree
+singleTermTests = testGroup "basic single terms"
   [ testCase "parse positive integer" $
       maybeParse "2" @?= Just (mkSFTerm 1 2 0)
   , testCase "parse negative integer" $
@@ -23,4 +26,28 @@ unitTests = testGroup "unit tests"
       maybeParse "-5e7" @?= Just (mkSFTerm 1 (-5) (-7))
   , testCase "parse sci-not float" $
       maybeParse "5.24e-2" @?= Just (mkSFTerm 3 524 4)
+  ]
+
+singleTermParenTests :: TestTree
+singleTermParenTests = testGroup "single terms with parens"
+  [ testCase "parse positive integer" $
+      maybeParse "(2)" @?= Just (mkSFTerm 1 2 0)
+  , testCase "parse negative integer w/ double parens" $
+      maybeParse "((-3))" @?= Just (mkSFTerm 1 (-3) 0)
+  , testCase "parse sci-not integer" $
+      maybeParse "(-5e7)" @?= Just (mkSFTerm 1 (-5) (-7))
+  , testCase "parse sci-not float w/ double parens" $
+      maybeParse "((5.24e-2))" @?= Just (mkSFTerm 3 524 4)
+  ]
+
+singleOpTests :: TestTree
+singleOpTests = testGroup "single operations"
+  [ testCase "parse integer addition" $
+      maybeParse "2 + 3" @?= Just (mkSFTerm 1 5 0)
+  , testCase "parse float subtraction" $
+      maybeParse "3.2 - 4.3" @?= Just (mkSFTerm 2 (-11) 1)
+  , testCase "parse multiplication" $
+      maybeParse "3 * 2" @?= Just (mkSFTerm 1 6 0)
+  , testCase "parse division with parens" $
+      maybeParse "(4 / 2) / 2" @?= Just (mkSFTerm 1 1 0)
   ]
