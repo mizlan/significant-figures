020824c2c160c33af25f456e2e7d98682ece20fc
Michael Lan <michaellan202@gmail.com>
2022-08-14 10:25:03 -0700

    refactor

--- tests/SigFigTest.hs
+++ tests/SigFigTest.hs
@@ -32,13 +32,13 @@ singleTermTests =
   testGroup
     "basic single terms"
     [ testCase "parse positive integer" $
-        maybeParseEval "2" @?= Just (mkSFMeasured 1 2 0),
+        parseEval "2" @?= Right (mkSFMeasured 1 2 0),
       testCase "parse negative integer" $
-        maybeParseEval "-3" @?= Just (mkSFMeasured 1 (-3) 0),
+        parseEval "-3" @?= Right (mkSFMeasured 1 (-3) 0),
       testCase "parse sci-not integer" $
-        maybeParseEval "-5e7" @?= Just (mkSFMeasured 1 (-5) (-7)),
+        parseEval "-5e7" @?= Right (mkSFMeasured 1 (-5) (-7)),
       testCase "parse sci-not float" $
-        maybeParseEval "5.24e-2" @?= Just (mkSFMeasured 3 524 4)
+        parseEval "5.24e-2" @?= Right (mkSFMeasured 3 524 4)
     ]
 
 singleConstantTests :: TestTree
@@ -46,13 +46,13 @@ singleConstantTests =
   testGroup
     "basic single constants"
     [ testCase "parse positive integer constant" $
-        maybeParseEval "2c" @?= Just (SFConstant 2),
+        parseEval "2c" @?= Right (SFConstant 2),
       testCase "parse negative integer constant" $
-        maybeParseEval "-3c" @?= Just (SFConstant (-3)),
+        parseEval "-3c" @?= Right (SFConstant (-3)),
       testCase "parse sci-not integer constant" $
-        maybeParseEval "-5e7c" @?= Just (SFConstant ((-5) * 10 ^ 7)),
+        parseEval "-5e7c" @?= Right (SFConstant ((-5) * 10 ^ 7)),
       testCase "parse sci-not float constant" $
-        maybeParseEval "5.24e-2c" @?= Just (SFConstant ((5.24 / 10 ^ 2) :: Rational))
+        parseEval "5.24e-2c" @?= Right (SFConstant ((5.24 / 10 ^ 2) :: Rational))
     ]
 
 singleTermParenTests :: TestTree
@@ -60,17 +60,17 @@ singleTermParenTests =
   testGroup
     "single terms with parens"
     [ testCase "parse positive integer" $
-        maybeParseEval "(2)" @?= Just (mkSFMeasured 1 2 0),
+        parseEval "(2)" @?= Right (mkSFMeasured 1 2 0),
       testCase "parse negative integer w/ double parens" $
-        maybeParseEval "((-3))" @?= Just (mkSFMeasured 1 (-3) 0),
+        parseEval "((-3))" @?= Right (mkSFMeasured 1 (-3) 0),
       testCase "parse sci-not integer w/ spaces" $
-        maybeParseEval "(-5e7   )" @?= Just (mkSFMeasured 1 (-5) (-7)),
+        parseEval "(-5e7   )" @?= Right (mkSFMeasured 1 (-5) (-7)),
       testCase "parse sci-not float w/ double parens and spaces" $
-        maybeParseEval "(  (5.24e-2) )" @?= Just (mkSFMeasured 3 524 4),
+        parseEval "(  (5.24e-2) )" @?= Right (mkSFMeasured 3 524 4),
       testCase "parse sci-not constant w/ spaces" $
-        maybeParseEval "(-.51e7c )" @?= Just (SFConstant $ (-51) * 10 ^ 5),
+        parseEval "(-.51e7c )" @?= Right (SFConstant $ (-51) * 10 ^ 5),
       testCase "parse sci-not constant w/ double parens and spaces" $
-        maybeParseEval "(  (2.e-2c) )" @?= Just (SFConstant 0.02)
+        parseEval "(  (2.e-2c) )" @?= Right (SFConstant 0.02)
     ]
 
 constantOpTests :: TestTree
@@ -78,11 +78,11 @@ constantOpTests =
   testGroup
     "constant operations"
     [ testCase "add two constant ints" $
-        maybeParseEval "2c + 25c" @?= Just (SFConstant 27),
+        parseEval "2c + 25c" @?= Right (SFConstant 27),
       testCase "add constant int and constant float" $
-        maybeParseEval "0.2c + 400c" @?= Just (SFConstant 400.2),
+        parseEval "0.2c + 400c" @?= Right (SFConstant 400.2),
       testCase "divide constant int and constant float" $
-        maybeParseEval "1c / 3.0c" @?= Just (SFConstant (1 % 3))
+        parseEval "1c / 3.0c" @?= Right (SFConstant (1 % 3))
     ]
 
 singleOpTests :: TestTree
@@ -90,15 +90,15 @@ singleOpTests =
   testGroup
     "single operations"
     [ testCase "parse integer addition" $
-        maybeParseEval "2 + 3" @?= Just (mkSFMeasured 1 5 0),
+        parseEval "2 + 3" @?= Right (mkSFMeasured 1 5 0),
       testCase "parse float subtraction" $
-        maybeParseEval "3.2 - 4.3" @?= Just (mkSFMeasured 2 (-11) 1),
+        parseEval "3.2 - 4.3" @?= Right (mkSFMeasured 2 (-11) 1),
       testCase "parse multiplication" $
-        maybeParseEval "3 * 2" @?= Just (mkSFMeasured 1 6 0),
+        parseEval "3 * 2" @?= Right (mkSFMeasured 1 6 0),
       testCase "parse division with parens" $
-        maybeParseEval "(4 / 2) / 2" @?= Just (mkSFMeasured 1 1 0),
+        parseEval "(4 / 2) / 2" @?= Right (mkSFMeasured 1 1 0),
       testCase "parse exponentiation" $
-        maybeParseEval "2 ** 3" @?= Just (mkSFMeasured 1 8 0)
+        parseEval "2 ** 3" @?= Right (mkSFMeasured 1 8 0)
     ]
 
 orderOfOperations :: TestTree
@@ -106,15 +106,15 @@ orderOfOperations =
   testGroup
     "order of operations"
     [ testCase "addition after multiplication" $
-        maybeParseEval "1 + 3 * 2" @?= Just (mkSFMeasured 1 7 0),
+        parseEval "1 + 3 * 2" @?= Right (mkSFMeasured 1 7 0),
       testCase "multiplication before subtraction" $
-        maybeParseEval "2.1 * 2.0 - 0.3" @?= Just (mkSFMeasured 2 39 1),
+        parseEval "2.1 * 2.0 - 0.3" @?= Right (mkSFMeasured 2 39 1),
       testCase "exp > mul > add" $
-        maybeParseEval "2.1 + 2.0 * 1.4 ** 2" @?= Just (mkSFMeasured 2 61 1),
+        parseEval "2.1 + 2.0 * 1.4 ** 2" @?= Right (mkSFMeasured 2 61 1),
       testCase "division after exponentiation" $
-        maybeParseEval "4 / 2 ** 2" @?= Just (mkSFMeasured 1 1 0),
+        parseEval "4 / 2 ** 2" @?= Right (mkSFMeasured 1 1 0),
       testCase "simple as can be" $
-        maybeParseEval "(2) * 4 - 1" @?= Just (mkSFMeasured 1 7 0)
+        parseEval "(2) * 4 - 1" @?= Right (mkSFMeasured 1 7 0)
     ]
 
 complexExpressions :: TestTree
@@ -122,21 +122,21 @@ complexExpressions =
   testGroup
     "complex expressions"
     [ testCase "complex 1 with rounding" $
-        maybeParseEval "(2 + 3.8 * 4.1) ** 2 - 20" @?= Just (mkSFMeasured 2 300 0),
+        parseEval "(2 + 3.8 * 4.1) ** 2 - 20" @?= Right (mkSFMeasured 2 300 0),
       testCase "mix constants with measured" $
-        maybeParseEval "2.0001 * 4c + 18.000007c" @?= Just (mkSFMeasured 6 260004 4),
+        parseEval "2.0001 * 4c + 18.000007c" @?= Right (mkSFMeasured 6 260004 4),
       testCase "mix constants with measured 2" $
-        maybeParseEval "4.01c + 28.4c + 18.12412" @?= Just (mkSFMeasured 7 5053412 5),
+        parseEval "4.01c + 28.4c + 18.12412" @?= Right (mkSFMeasured 7 5053412 5),
       testCase "constant division" $
-        maybeParseEval "2c/1c/2c/1c/2c * 8" @?= Just (mkSFMeasured 1 4 0),
+        parseEval "2c/1c/2c/1c/2c * 8" @?= Right (mkSFMeasured 1 4 0),
       testCase "constant division 2" $
-        maybeParseEval "2c/1c/2c/1c/2c * 8c" @?= Just (SFConstant 4),
+        parseEval "2c/1c/2c/1c/2c * 8c" @?= Right (SFConstant 4),
       testCase "interspersed constants and measured" $
-        maybeParseEval "((2c) + 3.1 * (4.7c) ** 1)" @?= Just (mkSFMeasured 2 17 0),
+        parseEval "((2c) + 3.1 * (4.7c) ** 1)" @?= Right (mkSFMeasured 2 17 0),
       testCase "simple addition" $
-        maybeParseEval "4 + 5 + 6" @?= Just (mkSFMeasured 2 15 0),
+        parseEval "4 + 5 + 6" @?= Right (mkSFMeasured 2 15 0),
       testCase "simple addition with mul" $
-        maybeParseEval "(4 + 5 + 6) * 1c" @?= Just (mkSFMeasured 2 15 0),
+        parseEval "(4 + 5 + 6) * 1c" @?= Right (mkSFMeasured 2 15 0),
       testCase "not-so-simple addition" $
-        maybeParseEval "(4 + 5 + 6) * 1" @?= Just (mkSFMeasured 1 20 0)
+        parseEval "(4 + 5 + 6) * 1" @?= Right (mkSFMeasured 1 20 0)
     ]
