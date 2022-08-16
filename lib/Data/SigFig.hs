0d84807f90ab529e1118b64d50156ac3d557b3a6
Michael Lan <michaellan202@gmail.com>
2022-08-16 11:04:08 -0700

    modify imports/exports [skip ci]

--- lib/Data/SigFig.hs
+++ lib/Data/SigFig.hs
@@ -4,11 +4,11 @@ module Data.SigFig (
   processExpression,
   parseEval,
   display,
+  evaluate,
   module Data.SigFig.Types
 ) where
 
 import Data.SigFig.Types
 import Data.SigFig.Util
-import Data.SigFig.Parse
 import Data.SigFig.Evaluate
 import Data.SigFig.Interface
