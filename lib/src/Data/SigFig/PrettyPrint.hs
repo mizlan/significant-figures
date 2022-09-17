88ae6951770867148975e9beb79cb8455996e564
Michael Lan <michaellan202@gmail.com>
2022-09-17 14:33:07 -0700

    seems like it works
    $ prettyPrint 0 $ mul [add [(lConstant 3.0),
      (lMeasured 1 4.2), mul [lConstant 3.88,
      lConstant 4.82]], sub [lConstant 4.7, lConstant 9.9]]
    "(3 + 4.2 + 3.88 * 4.82) * (4.7 - 9.9)"

--- lib/src/Data/SigFig/PrettyPrint.hs
+++ lib/src/Data/SigFig/PrettyPrint.hs
@@ -15,5 +15,30 @@ precedence = \case
   Prec2 {} -> 2
   Prec1 {} -> 1
 
+-- the only time we don't need parentheses is with leaf or if child is higher precedence
+
+precede :: Int -> Op -> Expr -> Text
+precede prec Add = (" + " <>) . prettyPrint prec
+precede prec Sub = (" - " <>) . prettyPrint prec
+precede prec Mul = (" * " <>) . prettyPrint prec
+precede prec Div = (" / " <>) . prettyPrint prec
+
 printTerm :: Term -> Text
 printTerm = display
+
+conditionallyAddParens :: Int -> Int -> Text -> Text
+conditionallyAddParens outer inner t = if inner > outer then t else "(" <> t <> ")"
+
+printFunc Log10 x = "log(" <> prettyPrint 0 x <> ")"
+printFunc Antilog10 x = "exp(" <> prettyPrint 0 x <> ")"
+
+prettyPrint :: Int -> Expr -> Text
+prettyPrint prec e =
+  let prec' = precedence e
+   in case e of
+        Literal n -> printTerm n
+        Prec1 ((_, x) : xs) -> conditionallyAddParens prec prec' $ prettyPrint 1 x <> foldMap (uncurry $ precede 1) xs
+        Prec2 ((_, x) : xs) -> conditionallyAddParens prec prec' $ prettyPrint 2 x <> foldMap (uncurry $ precede 2) xs
+        Exp a b -> conditionallyAddParens prec prec' $ prettyPrint 3 a <> " ** " <> prettyPrint 3 b
+        Apply a b -> printFunc a b
+        _ -> error "ill-formed expression"
