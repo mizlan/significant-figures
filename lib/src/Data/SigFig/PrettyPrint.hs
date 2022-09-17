69600db9426f09864ceb67f0c31a868b39e2dc08
Michael Lan <michaellan202@gmail.com>
2022-09-17 15:27:52 -0700

    support proper p-printing of literals

--- lib/src/Data/SigFig/PrettyPrint.hs
+++ lib/src/Data/SigFig/PrettyPrint.hs
@@ -1,12 +1,17 @@
+{-# LANGUAGE ImportQualifiedPost #-}
 {-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE OverloadedStrings #-}
 
 -- | A module to unparse an expression.
 module Data.SigFig.PrettyPrint (prettyPrint) where
 
-import Data.SigFig.Types
+import Data.BigDecimal (BigDecimal (..))
+import Data.BigDecimal qualified as BD
+import Data.SigFig.Types hiding (div)
+import Data.SigFig.Util (display, isTerminating)
 import Data.Text (Text)
-import Data.SigFig.Util (display)
+import Data.Text qualified as T
+import GHC.Real (Ratio(..))
 
 precedence = \case
   Apply {} -> 10
@@ -24,7 +29,31 @@ precede prec Mul = (" * " <>) . prettyPrint prec
 precede prec Div = (" / " <>) . prettyPrint prec
 
 printTerm :: Term -> Text
-printTerm = display
+printTerm (Measured sf bd) = format bd
+  where
+    ssf = T.pack $ show sf
+    format :: BigDecimal -> Text
+    format term' =
+      let term@(BigDecimal v s') = BD.nf term'
+          s = fromIntegral s' :: Integer
+          termText = T.pack . show $ term
+          p = fromIntegral (BD.precision term) :: Integer
+          rsdp = p - sf - s
+          rsd = if sf > p then 0 else v `div` (10 ^ (rsdp + s)) `mod` 10
+       in if rsd /= 0 || rsdp == 0 && p == 1
+            then termText
+            else
+              if rsdp >= 1
+                then let coef = BigDecimal v (fromIntegral (s + (p - 1))) in format coef <> "e" <> T.pack (show $ p - 1)
+                else
+                  termText
+                    <> (if s > 0 then "" else ".")
+                    <> T.replicate (fromIntegral $ sf - p) "0"
+printTerm (Constant v@(a :% b)) =
+  T.pack $
+    if isTerminating b
+      then (++ "c") . show . BD.nf $ fromRational v
+      else "(" ++ show a ++ "c / " ++ show b ++ "c)"
 
 conditionallyAddParens :: Int -> Int -> Text -> Text
 conditionallyAddParens outer inner t = if inner > outer then t else "(" <> t <> ")"
